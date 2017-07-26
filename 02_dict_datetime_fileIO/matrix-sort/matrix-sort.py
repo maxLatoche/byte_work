e0e57d3eff2cb6d350f12917ee81bfea0e97fb3d
Max Latoche <max.latoche@gmail.com>
2017-07-26 18:20:51 -0400

    completes credit card ex and adds more week1 folders

--- 02_dict_datetime_fileIO/matrix-sort/matrix-sort.py
+++ 02_dict_datetime_fileIO/matrix-sort/matrix-sort.py
@@ -1,22 +1,19 @@
 import numpy as np
-import re
 
 def matrix_ops(file_name):
-	#access file
+	# access file
 	with open(file_name) as fp:
-		#read lines
+		# read lines
 		l = fp.readlines()
-		#iterate over list and convert strings to numbers
+		# iterate over list and convert strings to numbers
 		for i in range(0,len(l)):
 			l[i] = l[i].split(" ")
-			#for j in range(0,len(l[i])):
-			#	int(l[i][j])
-			#print(l[i])
-		result = list(map(int(),l))
-		print(result)
-		#send list to array
-		#x = np.array(l)
-		#print(x)
+			for j in range(0,len(l[i])):
+				l[i][j] = int(l[i][j])
+		#print(l)
+		# send list to array
+		x = np.array(l)
+		print(x)
 		
 
 matrix_ops("../../../jng_instr_folder/week1/02_dict_datetime_fileIO/exercises/matrix-sort/testmatrix.txt")
